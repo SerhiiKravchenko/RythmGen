7ba76cfc18664ccf7ad0c8cb41071e274958c98c
Serhii Kravchenko <srh.kravchenko@gmail.com>
2019-04-01 11:18:47 +0300

    Initial Commit

---
+++ RythmGen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RythmGen
+//
+//  Created by Serhii Kravchenko on 4/1/19.
+//  Copyright © 2019 Serhii Kravchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
